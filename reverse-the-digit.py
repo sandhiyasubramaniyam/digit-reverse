c5486ca53da05a0eda777d5e1d5605e3670c2a96
sandhiyasubramaniyam <sandhiya.cs13@bitsathy.ac.in>
2016-06-07 15:31:35 +0530

    Create reverse-the-digit.py

---
+++ reverse-the-digit.py
@@ -0,0 +1,7 @@
+number=input("Enter the number")
+reverse=0
+while(number>0):
+ remainder=number%10
+ reverse=(reverse*10)+remainder
+ number=number/10
+print("Reverse of entered number is:",reverse)
